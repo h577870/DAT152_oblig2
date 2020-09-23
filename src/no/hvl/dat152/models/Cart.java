47ad74a62a63fbe245f72cf601b08a716a099dbf
christianfoleide <christian.foleide@gmail.com>
2020-09-24 01:10:25 +0200

    move from arraylist to map for product quantity

--- src/no/hvl/dat152/models/Cart.java
+++ src/no/hvl/dat152/models/Cart.java
@@ -1,21 +1,44 @@
 package no.hvl.dat152.models;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Cart {
-	
-	private ArrayList<Product> cartlist;
+		
+	private Map<Product,Integer> cart;
 	
 	public Cart() {
-		this.setCartlist(new ArrayList<Product>());
+		cart = new HashMap<Product, Integer>();
 	}
-
-	public ArrayList<Product> getCartlist() {
-		return cartlist;
+	
+	public Map<Product, Integer> getCart() {
+		return cart;
 	}
-
-	public void setCartlist(ArrayList<Product> cartlist) {
-		this.cartlist = cartlist;
+	
+	public void Add(Product p) {
+		if (cart.containsKey(p)) {
+			Integer prev = cart.get(p);
+			prev = prev.intValue();
+			System.out.println("Upping quantity of product " + p.getName() + " to " + prev);
+			cart.put(p, prev++);
+		} else {
+			System.out.println("Added new product " + p.getName() + " to the cart");
+			cart.put(p, 1);
+		}
+	}
+	
+	public int Quantity(Product p) {
+		return cart.get(p);
+	}
+	
+	public void Show() {
+		
+		cart.forEach((prod, quantity) -> {
+			
+			System.out.println("Name: " + prod.getName() + " | quantity: " + quantity + "\n");
+			
+		});
 	}
 
 }
